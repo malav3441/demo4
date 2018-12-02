a20d8ac7ac5a2d07b32c5def52ef459c28bb8b0e
malav3441 <45518283+malav3441@users.noreply.github.com>
2018-12-02 10:49:58 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,5 @@
+n = int(input("enter a number"))
+total = 0
+for i in range(n):
+    total += i
+print(total)
---
+++ squre.py
@@ -0,0 +1,6 @@
+number = list(range(1,21))
+squre = []
+for i in number:
+    squre.append(i**i)
+
+print(squre)
